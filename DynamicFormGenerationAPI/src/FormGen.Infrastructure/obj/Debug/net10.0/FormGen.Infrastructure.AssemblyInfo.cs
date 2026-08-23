c3797bc5f65bd2094b66dbb749163e58d7592318
Singh, Sachin <sachin.singh@sagitec.com>
2026-08-23 22:27:29 +0530

    Update Full Code

--- DynamicFormGenerationAPI/src/FormGen.Infrastructure/obj/Debug/net10.0/FormGen.Infrastructure.AssemblyInfo.cs
+++ DynamicFormGenerationAPI/src/FormGen.Infrastructure/obj/Debug/net10.0/FormGen.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormGen.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99310042ae9af192b4b98bb14fd388c0cf073378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5f55a85b35dcd1922af655443774e863d31c06")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormGen.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormGen.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
